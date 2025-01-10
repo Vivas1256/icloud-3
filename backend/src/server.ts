99e0568723aa8fa4edeaf7d7a7e495a184ceb3f7
vivas1256 <trikool1010@hotmail.com>
2025-01-09 23:42:09 -0500

    arreglo

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -7,45 +7,33 @@ import Company from "./models/Company";
 import { startQueueProcess } from "./queues";
 import { TransferTicketQueue } from "./wbotTransferTicketQueue";
 import cron from "node-cron";
-import bodyParser from "body-parser";
 
 const server = app.listen(process.env.PORT, async () => {
   const companies = await Company.findAll();
   const allPromises: any[] = [];
-  
   companies.map(async c => {
     const promise = StartAllWhatsAppsSessions(c.id);
     allPromises.push(promise);
   });
 
-  await Promise.all(allPromises);
-  startQueueProcess();
+  Promise.all(allPromises).then(() => {
+    startQueueProcess();
+  });
   logger.info(`Server started on port: ${process.env.PORT}`);
 });
 
-// Middleware para parsear JSON
-app.use(bodyParser.json());
-
-// Variable para almacenar la zona horaria del cliente
-let userTimezone = "UTC"; // Valor por defecto
-
-// Endpoint para recibir la zona horaria del cliente
-app.post('/api/set-timezone', (req, res) => {
-  userTimezone = req.body.timezone; // Almacena la zona horaria del cliente
-  logger.info(`Zona horaria del cliente establecida: ${userTimezone}`);
-  res.sendStatus(200);
-});
-
-// Tarea programada
 cron.schedule("* * * * *", async () => {
+
   try {
-    logger.info(`c: ${userTimezone}`);
-    
-    // Aquí puedes convertir la hora local a UTC si es necesario
+    // console.log("Running a job at 01:00 at America/Sao_Paulo timezone")
+    logger.info(`Serviço de transferencia de tickets iniciado`);
+
     await TransferTicketQueue();
-  } catch (error) {
+  }
+  catch (error) {
     logger.error(error);
   }
+
 });
 
 initIO(server);

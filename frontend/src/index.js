43057d94d4119f0645b16e65f77f39dd5af8c3ff
vivas1256 <trikool1010@hotmail.com>
2025-01-10 19:08:23 -0500

    actualiza

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,24 +1,19 @@
-console.log("Rendering App component...");
-console.log("Rendering CssBaseline component...");
-console.log("Rendering React app to DOM...");import React from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import CssBaseline from "@material-ui/core/CssBaseline";
-
 import App from "./App";
 
+// Mensajes de consola para depuración
+console.log("Rendering App component...");
+console.log("Rendering CssBaseline component...");
+console.log("Rendering React app to DOM...");
+
+// Renderiza la aplicación
 ReactDOM.render(
-	<CssBaseline>
-		<App />
-	</CssBaseline>,
-	document.getElementById("root")
+  <React.StrictMode>
+    <CssBaseline>
+      <App />
+    </CssBaseline>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// ReactDOM.render(
-// 	<React.StrictMode>
-// 		<CssBaseline>
-// 			<App />
-// 		</CssBaseline>,
-//   </React.StrictMode>
-
-// 	document.getElementById("root")
-// );

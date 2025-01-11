4608adf75b28f4ed4c6d56350d92cad115a7f919
vivas1256 <trikool1010@hotmail.com>
2025-01-10 23:24:37 -0500

    Actualizaciones

--- frontend/src/pages/Contacts/index.js
+++ frontend/src/pages/Contacts/index.js
@@ -337,7 +337,7 @@ function getDateLastMessage(contact) {
             {i18n.t("contacts.buttons.add")}
           </Button>
 
-         <CSVLink style={{ textDecoration:'none'}} separator=";" filename={'whaticket.csv'} data={contacts.map((contact) => ({ name: contact.name, number: contact.number, email: contact.email }))}>
+         <CSVLink style={{ textDecoration:'none'}} separator=";" filename={'Contactos.csv'} data={contacts.map((contact) => ({ name: contact.name, number: contact.number, email: contact.email }))}>
           <Button	variant="contained" color="primary"> 
           EXPORTAR CONTATOS 
           </Button>

7496590b3b4035a2306bb9895fbb361b0b926630
vivas1256 <trikool1010@hotmail.com>
2025-01-18 16:30:47 -0500

    actualiza

--- frontend/src/components/CampaignModal/index.js
+++ frontend/src/components/CampaignModal/index.js
@@ -9,7 +9,6 @@ import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
-import IconButton from "@material-ui/core/IconButton";
 import TextField from "@material-ui/core/TextField";
 import { green } from "@material-ui/core/colors";
 import { makeStyles } from "@material-ui/core/styles";
